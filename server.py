65bd6fb97af3a398f0f9cfd9c4f3900cc175aa71
Leonardo Machado <lhmg@hotmail.com>
2019-11-23 01:02:00 -0300

    Add a new router

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from werkzeug import secure_filename
 import cv2
 import os
 import app as comparacao
+import os.path 
+from os import path
 
 UPLOAD_FOLDER = './database'
 
@@ -38,10 +40,25 @@ def compare_file():
                 return 'Biometria confirmada!' + ' - Email:' + email
             return 'Biometria negada!'
         else:
-            return 'Email nao cadastrado'
+            return 'Email não cadastrado'
     else:
         return 'Algum campo não foi preenchido corretamente, por favor revise os mesmos e envie o formulário novamente!'
 
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit_finger():
+    email = request.form.get('email')
+    email2 = email.replace('@', '')
+    email2 = email2 + '.png'
+    file = request.files['file']
+    comp = str(path.exists('./database/' + email2))
+    if(comp != False):
+        filename = secure_filename(email2)
+        if (cv2.imread('database/'+ email2) is not None):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'Biometria Atualizada com sucesso'
+        else:
+            return 'Este e-mail não existe'
 
 if __name__ == "__main__":
     app.run()
